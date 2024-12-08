73d46bb52a58ae0358642abd7892117b768a2c2b
Johanna Kronqvist <johannakronqvist@MacBook-Air-som-tillhor-Johanna.local>
2024-12-08 10:29:32 +0100

    refactoring day 2

--- day-2/red-nosed-reports.js
+++ day-2/red-nosed-reports.js
@@ -2,40 +2,57 @@ const fs = require("fs");
 
 const rawData = fs.readFileSync("input.txt", "utf-8");
 
-//PART 1
-
 let reports = rawData
   .trim()
   .split("\n")
   .map((line) => line.split(" ").map(Number));
 
+console.log(reports);
+
 let safeReports = 0;
 
 function ascendingSafeReport(report) {
-  for (let i = 0; i < report.length; i++) {
+  let skippedOne = false;
+  for (let i = 0; i < report.length - 1; i++) {
     const dif = report[i + 1] - report[i];
     if (dif < 1 || dif > 3) {
-      return false;
+      if (skippedOne || !unsafeToSafeAscending(report, i)) {
+        return false;
+      }
+      skippedOne = true;
     }
   }
-
   return true;
 }
 
 function descendingSafeReport(report) {
-  for (let i = 0; i < report.length; i++) {
+  let skippedOne = false;
+  for (let i = 0; i < report.length - 1; i++) {
     const dif = report[i + 1] - report[i];
     if (dif > -1 || dif < -3) {
-      return false;
+      if (skippedOne || !unsafeToSafeDescending(report, i)) {
+        return false;
+      }
+      skippedOne = true;
     }
   }
-
   return true;
+}
+
+function unsafeToSafeAscending(report, value) {
+  const difSkipOne = report[value + 2] - report[value];
+  return difSkipOne >= 1 && difSkipOne <= 3;
+}
+
+function unsafeToSafeDescending(report, value) {
+  const difSkipOne = report[value + 2] - report[value];
+  return difSkipOne >= -3 && difSkipOne <= -1;
 }
 
 reports.forEach((report) => {
   if (ascendingSafeReport(report) || descendingSafeReport(report)) {
     safeReports += 1;
-    console.log(safeReports);
   }
 });
+
+console.log(safeReports);
--- day-3/mul-it-over.js
+++ day-3/mul-it-over.js
@@ -3,20 +3,29 @@ let fs = require("fs");
 const rawData = fs.readFileSync("input.txt", "utf-8");
 
 const mulPattern = /mul\([0-9]{1,3},[0-9]{1,3}\)/g;
+const combinedPattern = /(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))/g;
 
-let uncorruptedInstructions = rawData.match(mulPattern);
+let uncorruptedInstructions = rawData.match(combinedPattern);
+
+console.log(uncorruptedInstructions);
 
 let mulTotal = 0;
 let numPattern = /mul\((\d{1,3}),\s*(\d{1,3})\)/;
 
+let isEnabled = true;
+
 uncorruptedInstructions.forEach((mul) => {
-  let getNumbers = mul.match(numPattern);
+  if (mul.match(/don\'t\(\)/g)) isEnabled = false;
+  if (mul.match(/do\(\)/g)) isEnabled = true;
+  if (mul.match(numPattern) && isEnabled) {
+    let getNumbers = mul.match(numPattern);
 
-  let num1 = parseInt(getNumbers[1]);
-  let num2 = parseInt(getNumbers[2]);
-  let mulSum = num1 * num2;
+    let num1 = parseInt(getNumbers[1]);
+    let num2 = parseInt(getNumbers[2]);
+    let mulSum = num1 * num2;
 
-  mulTotal += mulSum;
+    mulTotal += mulSum;
+  }
 });
 
 console.log(mulTotal);
