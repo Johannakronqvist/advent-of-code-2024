b27ed2938cde3bedd4bcf6c45ca18efe38723fb7
Johanna Kronqvist <johannakronqvist@MacBook-Air-som-tillhor-Johanna.local>
2024-12-14 07:44:13 +0100

    part 1

--- day-7/bridge-repair.js
+++ day-7/bridge-repair.js
@@ -31,36 +31,17 @@ for (let i = 0; i < keys.length; i++) {
 function bridgeRepair(nums) {
   if (nums.length === 1) {
     if (nums[0] === key && addedToTotal === 0) {
-      console.log("added to total", addedToTotal);
       totalSum += key;
       addedToTotal = 1;
+      console.log(`TOTALSUM updated to ${totalSum} for key ${key}`);
     }
     return true;
   }
-  if (nums[0] >= key && nums.every((num, index) => index === 0 || num !== 1)) {
-    // Log the skipped path for debugging
-    console.log(`Skipping path: nums=${nums}, key=${key}`);
-  } else {
-    if (nums.length < 2) {
-      let concatination = concatenate(nums[0], nums[1]);
-      bridgeRepair([concatination, ...nums.slice(2)]);
-    }
 
-    let addition = nums[0] + nums[1];
-    bridgeRepair([addition, ...nums.slice(2)]);
+  let addition = nums[0] + nums[1];
+  bridgeRepair([addition, ...nums.slice(2)]);
 
-    let multiplication = nums[0] * nums[1];
-    bridgeRepair([multiplication, ...nums.slice(2)]);
-  }
-}
-
-function concatenate(a, b) {
-  // Convert numbers to strings
-  let parsedNum1 = a.toString();
-  let parsedNum2 = b.toString();
-
-  // Concatenate the strings and parse the result as an integer
-  let concatenatedNum = parseInt(parsedNum1 + parsedNum2);
-
-  return concatenatedNum;
+  let multiplication = nums[0] * nums[1];
+  bridgeRepair([multiplication, ...nums.slice(2)]);
+  //}
 }
